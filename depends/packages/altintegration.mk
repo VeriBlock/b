7e8f54c9b2f54c06ee8d4216187393078da7bad9
Overcooked Panda <overcookedpanda@gmail.com>
2020-04-18 09:40:12 -0500

    add alt-integration-cpp to depends, pull version from packages.mk

---
+++ depends/packages/altintegration.mk
@@ -0,0 +1,17 @@
+package=altintegration
+$(package)_version=c32afd4ed526b14631a4764ae5897e2e461cb4fe
+$(package)_download_path=https://github.com/VeriBlock/alt-integration-cpp/archive/
+$(package)_file_name=$($(package)_version).tar.gz
+$(package)_sha256_hash=543128f8483f8a6a4bb78f203e37989151463318d8de81cad3fb08ce1cb45d03
+
+define $(package)_config_cmds
+  cmake -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ -DCMAKE_INSTALL_PREFIX=$($(package)_staging_dir)$(host_prefix) -DCMAKE_BUILD_TYPE=Release -DTESTING=OFF -DWITH_ROCKSDB=OFF -DSHARED=OFF -B .
+endef
+
+define $(package)_build_cmds
+  $(MAKE)
+endef
+
+define $(package)_stage_cmds
+  $(MAKE) install
+endef
