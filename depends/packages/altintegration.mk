5eea4625eb7e113097c40de22947db861ba5249d
Bohdan Vanieiev <warchantua@gmail.com>
2020-04-20 14:58:19 +0300

    Update altintegration

--- depends/packages/altintegration.mk
+++ depends/packages/altintegration.mk
@@ -1,8 +1,8 @@
 package=altintegration
-$(package)_version=82c2cffe4cd87343a6a256934fd0694d2071a5c6
+$(package)_version=22a0d989bfcd49d6e60ce9a2153d5b25f04ef5ab
 $(package)_download_path=https://github.com/VeriBlock/alt-integration-cpp/archive/
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=820d689e4657cc248881931dc017bd06c75812535c090c9c396ea3d02a24d885
+$(package)_sha256_hash=771ddfa634ad6704990469e8fd881b4f7924c632aaa03fde1420adb81dc8d5f6
 
 define $(package)_config_cmds
   cmake -DCMAKE_C_COMPILER=$(CC) -DCMAKE_CXX_COMPILER=$(CXX) -DCMAKE_INSTALL_PREFIX=$($(package)_staging_dir)$(host_prefix) -DCMAKE_BUILD_TYPE=Release -DTESTING=OFF -DWITH_ROCKSDB=OFF -DSHARED=OFF -B .
