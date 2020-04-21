3fea7510b4d53f424118c08fdf6e03e209e6f3a7
Overcooked Panda <overcookedpanda@gmail.com>
2020-04-21 13:16:02 -0500

    Revert "improve solution per suggestion"
    This reverts commit 0ce9c538

--- depends/packages/altintegration.mk
+++ depends/packages/altintegration.mk
@@ -4,13 +4,15 @@ $(package)_download_path=https://github.com/VeriBlock/alt-integration-cpp/archiv
 $(package)_file_name=$($(package)_version).tar.gz
 $(package)_sha256_hash=9c2288e48a47ecb1c9d94c36f3acabffb91718d9c366110c52785563e4d0a355
 
-define $(package)_config_cmds
-  ifeq ($(HOST), x86_64-w64-mingw32)
+ifeq ($(HOST), x86_64-w64-mingw32)
+  define $(package)_config_cmds
     cmake -DCMAKE_C_COMPILER=$(HOST)-gcc -DCMAKE_CXX_COMPILER=$(HOST)-g++ -DCMAKE_INSTALL_PREFIX=$($(package)_staging_dir)$(host_prefix) -DCMAKE_BUILD_TYPE=Release -DTESTING=OFF -DWITH_ROCKSDB=OFF -DSHARED=OFF -B .
-  else
+  endef
+else
+  define $(package)_config_cmds
     cmake -DCMAKE_INSTALL_PREFIX=$($(package)_staging_dir)$(host_prefix) -DCMAKE_BUILD_TYPE=Release -DTESTING=OFF -DWITH_ROCKSDB=OFF -DSHARED=OFF -B .
-  endif
-endef
+  endef
+endif
 
 define $(package)_build_cmds
   $(MAKE)
