2e2a22ce7f3bb1c407b32f375cdf61513f68c93d
Overcooked Panda <overcookedpanda@gmail.com>
2020-04-24 10:41:38 -0500

    update licenses

--- src/vbk/pop_service_impl.cpp
+++ src/vbk/pop_service_impl.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2019-2020 Xenios SEZC
+// https://www.veriblock.org
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #include <vbk/pop_service_impl.hpp>
 
 #include <chrono>
