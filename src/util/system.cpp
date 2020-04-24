530f1c8c56ab838d3999ecb88bb0aa2cd19c17a4
Overcooked Panda <overcookedpanda@gmail.com>
2020-04-24 16:23:15 -0500

    update branding

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -565,7 +565,7 @@ fs::path GetDefaultDataDir()
     // Unix: ~/.bitcoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "vBitcoin";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -575,10 +575,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/vBitcoin";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / ".vbitcoin";
 #endif
 #endif
 }
