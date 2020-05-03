dfdb66b252fd68a3963a8faf67b387da1c226096
Bohdan Vanieiev <warchantua@gmail.com>
2020-05-03 16:55:58 +0300

    Allow VTB duplicates

--- depends/packages/altintegration.mk
+++ depends/packages/altintegration.mk
@@ -1,8 +1,8 @@
 package=altintegration
-$(package)_version=f0f9c89cad815cc77b8bd67b489a1d873c3711b9
+$(package)_version=f7962e828efcd63f084ea3659b0b3b170289c399
 $(package)_download_path=https://github.com/VeriBlock/alt-integration-cpp/archive/
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=011e0ce5be55e7bc37e30c4b5f3c5230c1b5946d7f0fe13cdd8b6f7d0c49e37d
+$(package)_sha256_hash=528227f66924162a270f0c7bb22e3555d5be9fe43c82a4e216464bbeb849901f
 
 ifeq ($(HOST), x86_64-w64-mingw32)
   define $(package)_config_cmds
