97735b221c1ca932aef594422dde5f62f0946ffa
Overcooked Panda <overcookedpanda@gmail.com>
2020-05-05 09:01:50 -0500

    fix macos cross compile build, add to ci

--- depends/packages/veriblock-pop-cpp.mk
+++ depends/packages/veriblock-pop-cpp.mk
@@ -8,6 +8,12 @@ ifeq ($(HOST), x86_64-w64-mingw32)
   define $(package)_config_cmds
     cmake -DCMAKE_C_COMPILER=$(HOST)-gcc -DCMAKE_CXX_COMPILER=$(HOST)-g++ -DCMAKE_INSTALL_PREFIX=$($(package)_staging_dir)$(host_prefix) -DCMAKE_BUILD_TYPE=Release -DTESTING=OFF -DWITH_ROCKSDB=OFF -DSHARED=OFF -B .
   endef
+else ifeq ($(HOST), x86_64-apple-darwin16)
+  define $(package)_config_cmds
+    cmake -DCMAKE_C_COMPILER=$(darwin_CC) -DCMAKE_CXX_COMPILER=$(darwin_CXX) -DCMAKE_INSTALL_PREFIX=$($(package)_staging_dir)$(host_prefix) -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES=x86_64 \
+    -DCMAKE_SYSTEM_NAME=Darwin -DCMAKE_SYSTEM_PROCESSOR=x86_64 -DCMAKE_C_COMPILER_TARGET=$(HOST) -DCMAKE_CXX_COMPILER_TARGET=$(HOST) -DCMAKE_OSX_SYSROOT=$(OSX_SDK) -DTESTING=OFF -DWITH_ROCKSDB=OFF \
+    -DSHARED=OFF -B .
+  endef
 else
   define $(package)_config_cmds
     cmake -DCMAKE_INSTALL_PREFIX=$($(package)_staging_dir)$(host_prefix) -DCMAKE_BUILD_TYPE=Release -DTESTING=OFF -DWITH_ROCKSDB=OFF -DSHARED=OFF -B .
