d3aa7602860eae9ad54e32490ee738e1a66d5541
Bohdan Vanieiev <warchantua@gmail.com>
2020-06-29 19:40:57 +0300

    Fix header

--- src/qt/trafficgraphwidget.cpp
+++ src/qt/trafficgraphwidget.cpp
@@ -6,10 +6,11 @@
 #include <qt/trafficgraphwidget.h>
 #include <qt/clientmodel.h>
 
-#include <QPainterPath>
 #include <QPainter>
 #include <QColor>
 #include <QTimer>
+#include <QPainterPath>
+
 
 #include <cmath>
 
