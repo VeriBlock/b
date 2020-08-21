cb5793014ca8dcf237efeed7523e7e0669031e60
Bohdan Vanieiev <warchantua@gmail.com>
2020-08-21 15:41:00 +0300

    1

--- depends/packages/veriblock-pop-cpp.mk
+++ depends/packages/veriblock-pop-cpp.mk
@@ -1,8 +1,8 @@
 package=veriblock-pop-cpp
-$(package)_version=34c50a4a9ed3a384cf54457dd41f761995cd37b7
+$(package)_version=d8bdaa90a2fffb685e7af2c393bf6b0215a467d1
 $(package)_download_path=https://github.com/VeriBlock/alt-integration-cpp/archive/
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=4a4d9f5194f4887216e2c32ee55fb4ec8029187b7504eb2915e7305e3ef4ebc9
+$(package)_sha256_hash=32b885fe1ffbc61044e38cbbb2e2159961209cd39933b63ff659dbe978734008
 $(package)_build_subdir=build
 $(package)_build_type=$(BUILD_TYPE)
 $(package)_asan=$(ASAN)
