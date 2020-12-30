0bf2d36848a416546848db6b03f50e386288f50b
Bohdan Vanieiev <warchantua@gmail.com>
2020-12-30 18:49:49 +0200

    Fix pop p2p tests

--- test/functional/feature_pop_p2p_ddos.py
+++ test/functional/feature_pop_p2p_ddos.py
@@ -21,6 +21,7 @@ from test_framework.mininode import (
 
 import time
 
+
 class BaseNode(P2PInterface):
     def __init__(self):
         super().__init__()
@@ -51,12 +52,13 @@ class PopP2P(BitcoinTestFramework):
 
         # endorse block 5
         addr = self.nodes[0].getnewaddress()
+        tipheight = self.nodes[0].getblock(self.nodes[0].getbestblockhash())['height']
         self.log.info("endorsing block 5 on node0 by miner {}".format(addr))
-        atv_id = endorse_block(self.nodes[0], self.apm, 5, addr)
+        atv_id = endorse_block(self.nodes[0], self.apm, tipheight - 5, addr)
 
         assert len(self.nodes[0].getpeerinfo()) == 1
         peerinfo = self.nodes[0].getpeerinfo()[0]
-        assert(not 'noban' in peerinfo['permissions'])
+        assert (not 'noban' in peerinfo['permissions'])
 
         # spaming node1
         self.log.info("spaming node0 ...")
@@ -82,12 +84,13 @@ class PopP2P(BitcoinTestFramework):
         # endorse block 5
         addr = self.nodes[0].getnewaddress()
         self.log.info("endorsing block 5 on node0 by miner {}".format(addr))
-        atv_id = endorse_block(self.nodes[0], self.apm, 5, addr)
+        tipheight = self.nodes[0].getblock(self.nodes[0].getbestblockhash())['height']
+        atv_id = endorse_block(self.nodes[0], self.apm, tipheight - 5, addr)
         raw_atv = self.nodes[0].getrawatv(atv_id)
 
         assert len(self.nodes[0].getpeerinfo()) == 1
         peerinfo = self.nodes[0].getpeerinfo()[0]
-        assert(not 'noban' in peerinfo['permissions'])
+        assert (not 'noban' in peerinfo['permissions'])
 
         # spaming node1
         self.log.info("spaming node0 ...")
@@ -113,12 +116,13 @@ class PopP2P(BitcoinTestFramework):
         # endorse block 5
         addr = self.nodes[0].getnewaddress()
         self.log.info("endorsing block 5 on node0 by miner {}".format(addr))
-        atv_id = endorse_block(self.nodes[0], self.apm, 5, addr)
+        tipheight = self.nodes[0].getblock(self.nodes[0].getbestblockhash())['height']
+        atv_id = endorse_block(self.nodes[0], self.apm, tipheight - 5, addr)
         raw_atv = self.nodes[0].getrawatv(atv_id)
 
         assert len(self.nodes[0].getpeerinfo()) == 1
         peerinfo = self.nodes[0].getpeerinfo()[0]
-        assert(not 'noban' in peerinfo['permissions'])
+        assert (not 'noban' in peerinfo['permissions'])
         assert peerinfo['banscore'] == 0
 
         # spaming node1
@@ -126,12 +130,11 @@ class PopP2P(BitcoinTestFramework):
         msg = msg_atv(raw_atv)
         # Send message is used to send a P2P message to the node over our P2PInterface
         self.nodes[0].p2p.send_message(msg)
-        
+
         time.sleep(1)
-        
+
         peerinfo = self.nodes[0].getpeerinfo()[0]
         assert peerinfo['banscore'] == 20
-
 
     def run_test(self):
         """Main test logic"""
