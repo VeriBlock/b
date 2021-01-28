84000a3c7cc2440fe2fb239ef33e6a14565c21ac
Bohdan Vanieiev <warchantua@gmail.com>
2021-01-28 16:34:49 +0200

    Bump alt-cpp

--- depends/packages/veriblock-pop-cpp.mk
+++ depends/packages/veriblock-pop-cpp.mk
@@ -1,8 +1,8 @@
 package=veriblock-pop-cpp
-$(package)_version=29a380d4275e91f6327bdd667307bd5b6fd860aa
+$(package)_version=7bf3e7b4ae4dc1f3fd42d1ac3e52258e6a30c1c7
 $(package)_download_path=https://github.com/VeriBlock/alt-integration-cpp/archive/
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=7346fda927ab716792202ee09d0ba17f0154d8aacaad5eed61eb1c9b671b8811
+$(package)_sha256_hash=a448efb120204b063fb7903b3e9f2e7b5ecbfead6fb9e6e98b7e8dc12b8a6aa5
 $(package)_build_subdir=build
 $(package)_build_type=$(BUILD_TYPE)
 $(package)_asan=$(ASAN)
