671ae6180f4e8f2a4ccd24528de037b53ee99561
Your Name <you@example.com>
2021-06-10 12:06:45 +0000

    Update depends

--- depends/packages/veriblock-pop-cpp.mk
+++ depends/packages/veriblock-pop-cpp.mk
@@ -11,31 +11,9 @@ define $(package)_preprocess_cmds
   mkdir -p build
 endef
 
-ifeq ($(strip $(HOST)),)
-  define $(package)_config_cmds
-    cmake -DCMAKE_INSTALL_PREFIX=$(host_prefix) -DCMAKE_BUILD_TYPE=$(package)_build_type \
-    -DTESTING=OFF -DSHARED=OFF -DASAN:BOOL=$(package)_asan ..
-  endef
-else ifeq ($(HOST), x86_64-apple-darwin16)
-  define $(package)_config_cmds
-    cmake -DCMAKE_C_COMPILER=$(darwin_CC) -DCMAKE_CXX_COMPILER=$(darwin_CXX) \
-    -DCMAKE_INSTALL_PREFIX=$(host_prefix) -DCMAKE_BUILD_TYPE=$(package)_build_type \
-    -DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_SYSTEM_NAME=Darwin -DCMAKE_SYSTEM_PROCESSOR=x86_64 \
-    -DCMAKE_C_COMPILER_TARGET=$(HOST) -DCMAKE_CXX_COMPILER_TARGET=$(HOST) \
-    -DCMAKE_OSX_SYSROOT=$(OSX_SDK) -DTESTING=OFF \
-    -DSHARED=OFF ..
-  endef
-else ifeq ($(HOST), x86_64-pc-linux-gnu)
-  define $(package)_config_cmds
-    cmake -DCMAKE_INSTALL_PREFIX=$(host_prefix) -DCMAKE_BUILD_TYPE=$(package)_build_type \
-    -DTESTING=OFF -DSHARED=OFF ..
-  endef
-else
-  define $(package)_config_cmds
-    cmake -DCMAKE_C_COMPILER=$(HOST)-gcc -DCMAKE_CXX_COMPILER=$(HOST)-g++ \
-    -DCMAKE_INSTALL_PREFIX=$(host_prefix) -DTESTING=OFF -DSHARED=OFF ..
-  endef
-endif
+define $(package)_config_cmds
+  $($(package)_cmake) -DCMAKE_INSTALL_PREFIX=$(host_prefix) -DCMAKE_BUILD_TYPE=$(package)_build_type -DTESTING=OFF -DSHARED=OFF ..
+endef
 
 define $(package)_build_cmds
   $(MAKE)
