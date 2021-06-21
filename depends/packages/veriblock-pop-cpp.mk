9f4eee2e60e26713047ca5faaca702e0920aa498
blackyblack <andrey_lekar@adoriasoft.com>
2021-06-21 12:28:33 +0000

    Fix

--- depends/packages/veriblock-pop-cpp.mk
+++ depends/packages/veriblock-pop-cpp.mk
@@ -17,13 +17,13 @@ define $(package)_preprocess_cmds
 endef
 
 define $(package)_set_vars
-  $(package)_ldflags_darwin+=-Wl,-stack_size -Wl,0x1000000
-  $(package)_ldflags_linux+=-Wl,-stack_size -Wl,0x1000000
-  $(package)_ldflags_mingw32+=-Wl,--stack,0x1000000
+  $(package)_ldflags_darwin+=-Wl,-stack_size -Wl,0x100000
+  $(package)_ldflags_linux+=-Wl,-stack_size -Wl,0x100000
+  $(package)_ldflags_mingw32+=-Wl,--stack,0x100000
 endef
 
 define $(package)_config_cmds
-  $($(package)_cmake) -DCMAKE_INSTALL_PREFIX=$(host_prefix) -DTESTING=OFF -DSHARED=OFF -DCMAKE_BUILD_TYPE=$($(package)_build_type) -DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_SYSTEM_PROCESSOR=x86_64 -DCMAKE_C_COMPILER_TARGET=$(HOST) -DCMAKE_CXX_COMPILER_TARGET=$(HOST) ..
+  $($(package)_cmake) -DTESTING=OFF -DSHARED=OFF -DCMAKE_BUILD_TYPE=$($(package)_build_type) ..
 endef
 
 define $(package)_build_cmds
