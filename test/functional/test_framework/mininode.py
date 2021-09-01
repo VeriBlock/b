73d071570bc8967a19b0f2ae02ae2ca7d5b2552f
Bohdan Vanieiev <warchantua@gmail.com>
2021-09-01 16:44:43 +0300

    Fix mainnet magic

--- test/functional/test_framework/mininode.py
+++ test/functional/test_framework/mininode.py
@@ -108,9 +108,10 @@ def calculate_network_magic(index):
 
 
 MAGIC_BYTES = {
-    "mainnet": calculate_network_magic(1),   # mainnet
+    "mainnet": bytes([ord(x) for x in 'vBTC']),   # mainnet
     "testnet3": calculate_network_magic(2),  # testnet3
     "regtest": calculate_network_magic(3),   # regtest
+    "detregtest": calculate_network_magic(3),   # detregtest
 }
 
 
