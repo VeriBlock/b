d307335a7c024b9c7acb42ccb96f2400aa867470
alex pozhilenkov <alex_pozhilenkov@adoriasoft.com>
2021-09-15 11:54:37 +0300

    fix feature_pop_p2p

--- test/functional/feature_pop_p2p.py
+++ test/functional/feature_pop_p2p.py
@@ -136,7 +136,7 @@ class PopP2P(BitcoinTestFramework):
         time.sleep(5)
 
         assert_equal(bn.executed_msg_atv, 1)
-        assert_equal(bn.executed_msg_offer_vbk, 2)
+        assert_equal(bn.executed_msg_offer_vbk, 1)
 
         self.log.info("_run_sync_after_generating successful")
 
