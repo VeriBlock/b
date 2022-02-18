4a1c4f7afe3235c1b5b6499988bdbf61a468be3c
Bohdan Vanieiev <warchantua@gmail.com>
2022-02-18 13:13:26 +0200

    fix

--- test/integration/btcsqd_node.py
+++ test/integration/btcsqd_node.py
@@ -230,7 +230,7 @@ class BTCSQdNode(Node):
             finalityDelay=s['finalityDelay'],
             keystoneInterval=s['keystoneInterval'],
             maxAltchainFutureBlockTime=s['maxAltchainFutureBlockTime'],
-            maxReorgDistance=s['maxReorgBlocks']
+            maxReorgBlocks=s['maxReorgBlocks']
         )
 
     def getrawatv(self, atvid: Hexstr) -> AtvResponse:
